627168b996d2f2e51e47bed428435926226d226c
Prasanna Gautam <prasannagautam10@gmail.com>
2026-04-04 17:13:24 +0545

    Simple Line Graph

---
+++ pyplotlearning.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt 
+
+x=['Mon','Tue','Wed','Thur','Fri']
+
+y=[20,7,14,18,10]
+
+plt.plot(x,y)
+plt.title("Bakkery Sales Last Week")
+
+plt.xlabel("Days")
+plt.ylabel('Sales Per Day')
+
+plt.show()
