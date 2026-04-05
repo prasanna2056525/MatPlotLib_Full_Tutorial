88c16175d01306c079ea88e4dd037d88025aee31
Prasanna Gautam <prasannagautam10@gmail.com>
2026-04-05 16:05:22 +0545

    Mat[lot lib

--- barchart.py
+++ barchart.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 product = ['A', "B", 'C', "D"]
 sales = [1000,800,500,1500]
 
-plt.hbar(product,sales, color= 'Blue', label="sales 2026")
+plt.bar(product,sales, color= 'Blue', label="sales 2026")
 plt.xlabel('Product')
 plt.ylabel("Total sales")
 plt.title("Sales of Product in 2026")
--- two scatterplot.py
+++ two scatterplot.py
@@ -4,7 +4,7 @@ hours_std = [1,2,3,4,5,6,7,8]
 marks1= [ 50,55,60,65,70,75,80,85]
 marks2 = [52,58,61,67,73,79,81,88]
 
-plt.scatter( hours_std,marks1, color="green", marker='^', label="Student Data Group 1")
+plt.scatter( hours_std,marks1, color="green", marker='^gi', label="Student Data Group 1")
 plt.scatter( hours_std,marks2, color="red", marker='o', label="Student Data Group 2")
 
 plt.xlabel('Hours Studied')
